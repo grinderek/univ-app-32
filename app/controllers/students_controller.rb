c4052e1007a32830f6f9bed665e29f46bdcd7b46
Danila Berazhny <dannmmaster@gmail.com>
2021-11-20 00:46:33 +0300

    Add create user action and display errors

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,18 @@ class StudentsController < ApplicationController
   end
 
   def create
-
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:success] = "You have successfully signed up"
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
+  private
+
+  def student_params
+    params.require(:student).permit(:name, email)
+  end
 end
