6d59bc6b318d49cd0841b448e620a0dcadbc09be
Danila Berazhny <dannmmaster@gmail.com>
2021-11-20 00:40:21 +0300

    Build sign up form

---
+++ app/models/student.rb
@@ -0,0 +1,9 @@
+class Student < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :email, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: { with: URI::MailTo::EMAIL_REGEXP }
+
+end
