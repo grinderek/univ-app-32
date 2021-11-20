7ead1c1179b1a336252b434b230bdfb77bc84601
Danila Berazhny <dannmmaster@gmail.com>
2021-11-20 03:09:30 +0300

    Add association from front end

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def session_link
     if logged_in?
-      link_to "Log out", logout_path, method::delete
+      link_to "Log out", logout_path, method: :delete
     else
       link_to "Log in", login_path
     end
